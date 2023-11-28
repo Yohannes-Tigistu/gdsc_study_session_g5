9c835d258099be5fe0176d2e9970893726f60375
yohannestigistu1@gmail.com <125863756+chiefo01@users.noreply.github.com>
2023-11-28 09:25:16 +0300

    Modified naming scheme

--- list_odd_numbers/index.js
+++ list_odd_numbers/index.js
@@ -1,14 +1,14 @@
-function factorial(bound){
-  let count=0;
-  let array=[];
+function paritycheck(bound){
+  let oddNumList=[];
+  let index=0;
   for(let i=1;i<bound;i++){
      if((i%2!=0)){
-      array[count]=i;
-      count++;
-     }
-  }
- return array;
-} 
+      oddNumList[index]=i;
+      index++;
+    }
+  } 
+  return  oddNumList;
+}
   const upperbound = prompt("please enter the number");
-  const value = factorial(upperbound);
-  alert("The odd numbers are:"+value);
+  const list =paritycheck(upperbound);
+  alert("The odd numbers are:"+list);
